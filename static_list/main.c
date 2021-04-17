cf8871639629da73abedc96df11d12b5a1f8672f
4ly-a <4ly.alcantara@gmail.com>
2021-04-17 12:06:57 -0300

    static list

---
+++ static_list/main.c
@@ -0,0 +1,26 @@
+#include "li_estatica.h"
+
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+
+	printf("Funcionou :)\n");
+
+	// Lista *li;	
+	
+	// li = cria_lista();
+
+	// printf("\nO tamanho da lista é: %d", tamanho_lista(li));
+
+	// Aluno dados_aluno;
+	// strcpy(dados_aluno.nome, "Alysson");
+	// dados_aluno.matricula = 10848470;  
+
+	// // insere_lista_final(li, dados_aluno);
+
+	// insere_lista_inicio(li, dados_aluno);
+
+
+	// libera_lista(li);
+
+	return 0;
+}
